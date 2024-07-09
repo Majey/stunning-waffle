524f67d869c469a3638ce2b90b21e789e6bf0625
Majey <kevinerick78@yahoo.com>
2024-07-09 18:23:46 +0300

    Making API call using useEffect

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,19 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "7204dcc697f2478dab01af6cfe1350cf";
+
 const Search = () => {
   const [query, setQuery] = useState("Pizza");
 
   useEffect(() => {
-    const demo = () => {
-      console.log("Use effect fired");
+    const fetchFood = async () => {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results)
     };
-    demo();
+    fetchFood();
   }, [query]);
 
   return (
